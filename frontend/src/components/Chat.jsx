d05730694eef1f6b7882e40b6ddb82cf4a079ec3
Susana Chang <samanta.chang@utec.edu.pe>
2023-10-25 18:12:51 -0500

    styles chat

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -3,6 +3,8 @@ import { useState, useEffect } from 'react';
 import "../css/Chat.css"
 import url from './url.js';
 
+import LogoEnviar from '../icons/enviar.png'
+
 const headers = {
   'Content-Type': 'application/json',
 };
@@ -86,31 +88,47 @@ const Chat = () => {
 
     return (
         <div>
-          <div>
-              {chats.map((chat) => (
-                  <div key = {chat.uuid}>
-                      {getButtonEmail(chat)}
-                  </div>
-              ))}
-            </div>
-            <div>
-              {renderChat.map((chat) => (
-                <div key="1">
-                  <div>{getEmail(chat)}</div>
-                  <div className="contenedor-mensajes">
-                    {chat.messages.map((message) => (
-                      <div>
-                      <div key={message.date + message.user} className={lugarMensaje(chat, message)}>
-                        <div className="usuario">{message.user}</div>
-                        <div className="contenido">{message.content}</div>
-                        <div className="fecha">{message.date}</div>
-                      </div>
-                      </div>
-                    ))}
-                  </div>
+          <div className="contenedor-mas-grande-chat">
+            <div className="contenedor-grande-chat">
+
+              <div className="contenedor-izq-chat">
+                <div className="texto-chats-chat">CHATS</div>
+                <div className="contenedor-emails-chat">
+                  {chats.map((chat) => (
+                    <div key = {chat.uuid}>
+                        {getButtonEmail(chat)}
+                    </div>
+                  ))}
                 </div>
-              ))}
+              </div>
+              <div className="contenedor-der-chat">
+                <div className="contenedor-vacio-chat">
+                <div className="vacio-chat" />
+
+                {renderChat.map((chat) => (
+                  <div key={chat.uuid} className="contenedor-email-mensajes">
+                    <div className="email-seleccionado">{getEmail(chat)}</div>
+                    <div className="contenedor-mensajes">
+                      {chat.messages.map((message) => (
+                        <div key={message.uuid}>
+                          <div className={lugarMensaje(chat, message)}>
+                            <div className="usuario">{message.user}</div>
+                            <div className="contenido">{message.content}</div>
+                            <div className="fecha">{message.date}</div>
+                          </div>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                ))}
+                </div>
+                <div className="contenedor-enviar">
+                  <input className="input-texto-chat" type="text" />
+                  <img src={LogoEnviar} height={25} width={25}/>
+                </div>
+              </div>
             </div>
+          </div>
         </div>
     )
 }
