2f5e99b041cba3ac3ccd7d63466534fc065e16a0
Susana Chang <samanta.chang@utec.edu.pe>
2023-11-15 10:22:52 -0500

    F botones descarga

--- frontend/src/components/Admin.jsx
+++ frontend/src/components/Admin.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { useState } from 'react';
 import { CSVLink, CSVDownload } from "react-csv";
 import { url } from './utils.js'
+import "../css/Admin.css"
 
 function Admin() {
     const [csvData, setCsvData] = useState([]);
     const [filename, setFilename] = useState('my-file.csv');
+    const [show, setShow] = useState(false);
 
     function descargarDesdeDynamoDB(endpoint) {
         const lambdaURL = url + '/csv/' + endpoint;
@@ -16,21 +18,30 @@ function Admin() {
             console.log(data);
             setCsvData(data);
             setFilename(endpoint + '.csv')
+            setShow(true);
           })
     }
 
     return (
       <div>
-        <button onClick={() => descargarDesdeDynamoDB('register')}>Fecha Creación Usuarios</button>
-        <button onClick={() => descargarDesdeDynamoDB('event')}>Fecha Creación Eventos</button>
-        <button onClick={() => descargarDesdeDynamoDB('qualification')}>Calificaciones Mentor Match</button>
-        <button onClick={() => descargarDesdeDynamoDB('login')}>Login Usuarios</button>
-        <button onClick={() => descargarDesdeDynamoDB('state')}>Cambio de Estado</button>
+        <div className='contenedor-boton-descarga'>
+          <button className="boton-descarga" onClick={() => descargarDesdeDynamoDB('register')}>Fecha Creación Usuarios</button>
+          <button className="boton-descarga" onClick={() => descargarDesdeDynamoDB('event')}>Fecha Creación Eventos</button>
+          <button className="boton-descarga" onClick={() => descargarDesdeDynamoDB('qualification')}>Calificaciones Mentor Match</button>
+          <button className="boton-descarga" onClick={() => descargarDesdeDynamoDB('login')}>Login Usuarios</button>
+          <button className="boton-descarga" onClick={() => descargarDesdeDynamoDB('state')}>Cambio de Estado</button>
+        </div>
+        {
+          show && <div className='contenedor-boton-descarga-15'>
           <CSVLink
-            data={csvData}
-            enclosingCharacter={`'`}
-            filename={filename}
-          >Download me</CSVLink>
+              onClick={() => setShow(false)}
+              className='boton-descarga-15'
+              data={csvData}
+              enclosingCharacter={`'`}
+              filename={filename}
+            >Descargar .cvs</CSVLink>
+          </div>
+        }
       </div>
     );
 }
